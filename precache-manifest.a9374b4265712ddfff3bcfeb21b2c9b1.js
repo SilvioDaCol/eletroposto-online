c7bd91a754492974434b448d58d8942e65655b23
Silvio da Col <silvio.brito@techplus.com.br>
2020-09-29 21:36:49 -0300

    Updates

---
+++ precache-manifest.a9374b4265712ddfff3bcfeb21b2c9b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0388908d894c76d1f379300ad5712918",
+    "url": "/eletroposto-online/index.html"
+  },
+  {
+    "revision": "7f38bb67707b59664bfc",
+    "url": "/eletroposto-online/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "29c77ec54d9428499776",
+    "url": "/eletroposto-online/static/css/main.bf4bfdc5.chunk.css"
+  },
+  {
+    "revision": "7f38bb67707b59664bfc",
+    "url": "/eletroposto-online/static/js/2.b1581098.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/eletroposto-online/static/js/2.b1581098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c77ec54d9428499776",
+    "url": "/eletroposto-online/static/js/main.362b52ce.chunk.js"
+  },
+  {
+    "revision": "460fd34da2f01a4758d0",
+    "url": "/eletroposto-online/static/js/runtime-main.d7fdd0fa.js"
+  },
+  {
+    "revision": "c6d5babe6119e8462cdeba14e90bf702",
+    "url": "/eletroposto-online/static/media/reciclar.c6d5babe.svg"
+  },
+  {
+    "revision": "b6b1d1e31903858b17e2b9a3c8235b2d",
+    "url": "/eletroposto-online/static/media/star.b6b1d1e3.png"
+  }
+]);
